72e8fec613f21d449188846f6c23bb90b93d8f8b
Anthy Chen <koyun.chen@gmail.com>
2023-03-10 11:53:50 -0800

    Initial commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Anthy Chen on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
