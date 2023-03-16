e37c5be8dfaed774b73479ac722e562aed511524
Anthy Chen <koyun.chen@gmail.com>
2023-03-16 15:50:26 -0700

    Added conversion for volume, time, temperature

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -7,19 +7,31 @@
 
 import SwiftUI
 
+enum Conversions: String {
+    case Distance,Volume,Temperature, Time
+}
+
 struct ContentView: View {
     
+    @State private var selected_conversion_index = 0
     @State private var input_value = 0.0
-    @State private var input_unit = UnitLength.meters
-    @State private var output_unit = UnitLength.kilometers
+    @State private var input_unit: Dimension = UnitLength.meters
+    @State private var output_unit: Dimension = UnitLength.kilometers
     @FocusState private var inputIsFocused: Bool
     
-    let unit_selection: [UnitLength] = [.feet, .meters, .kilometers, .miles, .yards]
+    let conversions: [Conversions] = [.Distance, .Volume, .Temperature, .Time]
+    let unit_types = [
+        Conversions.Distance    : [UnitLength.meters, UnitLength.kilometers, UnitLength.miles, UnitLength.inches, UnitLength.feet],
+        Conversions.Volume      : [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds],
+        Conversions.Temperature : [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        Conversions.Time        : [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds, UnitDuration.milliseconds, UnitDuration.microseconds]
+    ]
+    
     let formatter: MeasurementFormatter
     init() {
         formatter = MeasurementFormatter()
         formatter.unitOptions = .providedUnit
-        formatter.unitStyle = .long
+        formatter.unitStyle = .medium
     }
     var output: String {
         let input_measurement = Measurement(value: input_value, unit: input_unit)
@@ -38,14 +50,20 @@ struct ContentView: View {
                     Text("Value to conver")
                 }
                 
+                Picker("Conversion", selection: $selected_conversion_index) {
+                    ForEach(0..<conversions.count) {
+                        Text(conversions[$0].rawValue)
+                    }
+                }
+                
                 Picker("Convert from", selection: $input_unit) {
-                    ForEach(unit_selection, id: \.self) {
+                    ForEach(unit_types[conversions[selected_conversion_index]]!, id: \.self) {
                         Text(formatter.string(from: $0).capitalized)
                     }
                 }
                 
                 Picker("Convert to", selection: $output_unit) {
-                    ForEach(unit_selection, id: \.self) {
+                    ForEach(unit_types[conversions[selected_conversion_index]]!, id: \.self) {
                         Text(formatter.string(from: $0).capitalized)
                     }
                 }
@@ -64,6 +82,11 @@ struct ContentView: View {
                         inputIsFocused = false
                     }
                 }
+            }
+            .onChange(of: selected_conversion_index) { new_selection_index in
+                let units = unit_types[conversions[new_selection_index]]
+                input_unit = units![0]
+                output_unit = units![1]
             }
         }
     }
